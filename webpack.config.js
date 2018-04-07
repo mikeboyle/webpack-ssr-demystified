af891430ad4dab8df77cec6437812d871da1d76a
Mike Boyle <mikesboyle@gmail.com>
2018-04-07 13:18:46 -0400

    006: Loaders transpile and compile your code, then give it to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
 
 module.exports = {
+  devtool: "source-map",
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["react", "env"]
+        }
+      }
+    ]
   },
   resolve: {
     extensions: [".js", ".jsx"]
