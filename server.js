e026fafd6a12c092ea4eab88c7cb018efe02512b
Mike Boyle <mikesboyle@gmail.com>
2018-04-07 11:08:42 -0400

    001: Basic Web server that returns an HTML file

---
+++ server.js
@@ -0,0 +1,11 @@
+const Express = require("express");
+const path = require("path");
+const server = Express();
+
+server.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+server.listen(3000, function() {
+  console.log('Server listening on port 3000...');
+});
